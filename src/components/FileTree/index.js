6f7bec341d5bbc1d121a10c956aceb841944fe04
lucas sandeville <lucas.sandeville@gmail.com>
2020-05-02 17:45:08 -0300

    improve file tree display

--- src/components/FileTree/index.js
+++ src/components/FileTree/index.js
@@ -16,7 +16,7 @@ function FileTree(props) {
     <FileTreeContainer>
       <code>
         {lines.map(line => (
-          <FileTreeLine key={line}>{line}</FileTreeLine>
+          <FileTreeLine key={line}>{line.substring(4)}</FileTreeLine>
         ))}
       </code>
     </FileTreeContainer>
