7530d794e53164da48a603414a0d2759a9f76195
lucas sandeville <lucas.sandeville@gmail.com>
2020-05-11 19:07:32 -0300

    make folder inputs allow nesting

--- src/components/FilesForm/InputLine.js
+++ src/components/FilesForm/InputLine.js
@@ -4,10 +4,11 @@ import { InputContainer, Input, MidDot } from "./styles";
 import { FiChevronDown, FiFolder, FiFile } from "react-icons/fi";
 import { useFileTree } from "../../hooks/fileTree";
 
-export default function InputLine({ type, col, initialValue, nodePath }) {
+export default function InputLine({ type, col, initialValue, nodePath, children }) {
   const [value, setValue] = useState(initialValue);
   const { updateFileTree } = useFileTree();
   const inputRef = React.useRef();
+  const divRef = React.useRef();
 
   const handleChange = useCallback(() => {
     const newValue = inputRef.current.value;
@@ -18,24 +19,34 @@ export default function InputLine({ type, col, initialValue, nodePath }) {
 
   if (type === "folder") {
     return (
-      <InputContainer col={col}>
-        <FiChevronDown style={{ opacity: 0.8 }} />
-        <FiFolder style={{ opacity: 0.8 }} />
-        <Input
-          ref={inputRef}
-          placeholder="Folder name"
-          value={value}
-          onChange={handleChange}
-        ></Input>
-      </InputContainer>
+      <div id={nodePath} ref={divRef}>
+        <InputContainer col={col}>
+          <FiChevronDown style={{ opacity: 0.8 }} />
+          <FiFolder style={{ opacity: 0.8 }} />
+          <Input
+            ref={inputRef}
+            placeholder="Folder name"
+            value={value}
+            onChange={handleChange}
+          ></Input>
+        </InputContainer>
+        {children}
+      </div>
     );
   } else if (type === "file") {
     return (
-      <InputContainer col={col}>
-        <MidDot>&#183;</MidDot>
-        <FiFile style={{ opacity: 0.8 }} />
-        <Input ref={inputRef} placeholder="File name" value={value} onChange={handleChange}></Input>
-      </InputContainer>
+      <div id={nodePath} ref={divRef}>
+        <InputContainer col={col}>
+          <MidDot>&#183;</MidDot>
+          <FiFile style={{ opacity: 0.8 }} />
+          <Input
+            ref={inputRef}
+            placeholder="File name"
+            value={value}
+            onChange={handleChange}
+          ></Input>
+        </InputContainer>
+      </div>
     );
   }
 }
--- src/components/FilesForm/index.js
+++ src/components/FilesForm/index.js
@@ -10,12 +10,15 @@ function FilesForm() {
 
   return (
     <FilesFormContainer>
-      <InputLine type="folder" col="0" nodePath="root" initialValue='exampleApp'/>
-      <InputLine type="folder" col="1" nodePath="0" initialValue='folder' />
-      <InputLine type="folder" col="2" nodePath="0-0" initialValue='folder(1)' />
-      <InputLine type="file" col="3" nodePath="0-0-0" initialValue='example.css' />
-      <InputLine type="file" col="2" nodePath="0-1" initialValue='example.html' />
-      <InputLine type="file" col="1" nodePath="1" initialValue='example.js' />
+      <InputLine type="folder" col="0" nodePath="root" initialValue="exampleApp">
+        <InputLine type="folder" col="1" nodePath="0" initialValue="folder">
+          <InputLine type="folder" col="2" nodePath="0-0" initialValue="folder(1)">
+            <InputLine type="file" col="3" nodePath="0-0-0" initialValue="example.css" />
+          </InputLine>
+          <InputLine type="file" col="2" nodePath="0-1" initialValue="example.html" />
+        </InputLine>
+        <InputLine type="file" col="1" nodePath="1" initialValue="example.js" />
+      </InputLine>
     </FilesFormContainer>
   );
 }
